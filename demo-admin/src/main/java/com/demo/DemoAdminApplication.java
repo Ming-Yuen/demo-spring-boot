8c8c3dae485dfe1a1d9271921d018f73f5630750
Ming Yuen <toyuenkaming@gmail.com>
2024-08-25 16:42:42 +0800

    spring cloud

--- demo-admin/src/main/java/com/demo/DemoAdminApplication.java
+++ demo-admin/src/main/java/com/demo/DemoAdminApplication.java
@@ -1,12 +1,13 @@
 package com.demo;
-import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 @SpringBootApplication
 @EnableWebMvc
 @EnableCaching
+@EnableDiscoveryClient
 public class DemoAdminApplication {
     public static void main(String[] args) {
         SpringApplication.run(DemoAdminApplication.class, args);
